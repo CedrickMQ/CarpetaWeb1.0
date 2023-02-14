3671d25f00c5939d3e8bd3117db76fc81dd1b65b
CedrickMQ <cedrick.marcial25@unach.mx>
2023-02-13 19:40:07 -0600

    final

--- __main__/App.py
+++ __main__/App.py
@@ -5,26 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-#    
-#@app.route('/Especialidades.html')
-#def Especialidades():
-#    return render_template('Especialidades.html')
 
-#@app.route('/Servicios.html')
-#def Servicios():
-#    return render_template('Servicios.html')
-
-#@app.route('/Quienes Somos.html')
-#def Quienes_Somos():
-#    return render_template('Quienes Somos.html')
-
-#@app.route('/Contacto.html')
-#def Contacto():
-#    return render_template('Contacto.html')
-
-#@app.route('/Sesion.html')
-#def Sesion():
-#    return render_template('Sesion.html')
+@app.route('/templates/login.html')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug= True, port=4000, host="0.0.0.0")
