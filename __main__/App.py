d8775f2d12a3b2e7ed352ccb15dcfe2cce02840f
CedrickMQ <112716158+CedrickMQ@users.noreply.github.com>
2023-03-28 16:46:42 -0600

    Ya sube datos en Mysql

--- __main__/App.py
+++ __main__/App.py
@@ -1,29 +1,56 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,flash, session, send_file
+from config import database
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
+app.secret_key = "NUT_NUT"
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        Nombre = request.form['Nombre']
-        Email = request.form['Email']
-        next = request.args.get('next', None)
-        if next:
-            return redirect(next)
-        return redirect(url_for('inicio'))
-    return render_template('index.html')
+    return redirect(url_for('inicio'))
 
-@app.route('/templates/index.html')
+@app.route('/templates/index.html', methods=['GET', 'POST'])
 def inicio():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return render_template ('index.html')
+    else:
+        return render_template ('index.html')
 
-@app.route('/templates/login.html')
+@app.route('/templates/login.html',methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        usuario = request.form['Usuario']
+        contrasena = request.form['password']
+        cursor = database.cursor()
+        cursor.execute('SELECT * FROM Usuarios_v2 WHERE Usuario = %s AND Contraseña = %s', (usuario, contrasena))
+        cuenta = cursor.fetchone()
+        if cuenta:
+            session['Usuario'] = cuenta[1]
+            flash ('Bienvenido')
+            return redirect(url_for('inicio'))
+        else:
+            return render_template('login.html', error='Usuario o contraseña incorrecta.')
+    else:
+        return render_template('login.html')
 
-@app.route('/static/Sql/Login.php')
-def loginPhp():
-    return render_template('Login.php')
+@app.route('/templates/Formulario.html',methods=['GET', 'POST'])
+def Registro():
+    return render_template ('Formulario.html')
+
+@app.route('/Formulario', methods=['GET','POST'])
+def Form():
+    if request.method == 'POST':
+
+        Usuario = request.form['Usuario']
+        Password = request.form['password']
+        Fullname = request.form['Nombre']
+
+        cursor = database.cursor(dictionary=True)
+        cursor.execute('INSERT INTO Usuarios_v2 (Usuario, Contraseña, NombreCompleto) VALUES (%s, %s, %s);', (Usuario,Password,Fullname))
+        database.commit()
+        return redirect(url_for('Registro'))
+    else:
+        return redirect(url_for('Registro'))
 
 if __name__ == '__main__':
     app.run(debug= True, port=4000, host="0.0.0.0")
